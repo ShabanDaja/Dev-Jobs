87f7c559b03234506526188314c7f489de7e3a54
Shaban Daja <shabandaja710@gmail.com>
2023-10-02 12:08:04 +0200

    fetch job item data

--- index.js
+++ index.js
@@ -1,66 +1,72 @@
 // GLOBAL
-const bookmarksBtnEl = document.querySelector('.bookmarks-btn');
-const errorEl = document.querySelector('.error');
-const errorTextEl = document.querySelector('.error__text');
-const jobDetailsEl = document.querySelector('.job-details');
+const bookmarksBtnEl = document.querySelector(".bookmarks-btn");
+const errorEl = document.querySelector(".error");
+const errorTextEl = document.querySelector(".error__text");
+const jobDetailsEl = document.querySelector(".job-details");
 const jobDetailsContentEl = document.querySelector(".job-details__content");
-const jobListBookmarksEl = document.querySelector('.job-list--bookmarks');
+const jobListBookmarksEl = document.querySelector(".job-list--bookmarks");
 const jobListSearchEl = document.querySelector(".job-list--search");
 const numberEl = document.querySelector(".count__number");
 const paginationEl = document.querySelector(".pagination");
 const paginationBtnNextEl = document.querySelector(".pagination__button--next");
 const paginationBtnBackEl = document.querySelector(".pagination__button--back");
-const paginationNumberNextEl = document.querySelector(".pagination__number--next");
-const paginationNumberBackEl = document.querySelector(".pagination__number--back");
+const paginationNumberNextEl = document.querySelector(
+  ".pagination__number--next"
+);
+const paginationNumberBackEl = document.querySelector(
+  ".pagination__number--back"
+);
 const searchFormEl = document.querySelector(".search");
 const searchInputEl = document.querySelector(".search__input");
 const sortingEl = document.querySelector(".sorting");
-const sortingBtnRelevantEl = document.querySelector(".sorting__button--relevant");
+const sortingBtnRelevantEl = document.querySelector(
+  ".sorting__button--relevant"
+);
 const sortingBtnRecentEl = document.querySelector(".sorting__button--recent");
 const spinnerSearchEl = document.querySelector(".spinner--search");
 const spinnerJobDetailsEl = document.querySelector(".spinner--job-details");
 
 // search component
-const submitHandler = event => {
-    // prevent default behavior
-    event.preventDefault();
+const submitHandler = (event) => {
+  // prevent default behavior
+  event.preventDefault();
 
-    //search text
-    const searchText = searchInputEl.value;
+  //search text
+  const searchText = searchInputEl.value;
 
-    //validation
-    const forbiddenPattern = /[0-9]/;
-    const patternMatch = forbiddenPattern.test(searchText);
-    if (patternMatch) {
-        errorTextEl.textContent = 'Your search may not contain numbers';
-        errorEl.classList.add('error--visible');
-        setTimeout(() => {
-            errorEl.classList.remove('error--visible');
-        }, 3500);
-    }
-    //blur input
-    searchInputEl.blur();
-    jobListSearchEl.innerHTML = '';
-    spinnerSearchEl.classList.add('spinner--visible');
+  //validation
+  const forbiddenPattern = /[0-9]/;
+  const patternMatch = forbiddenPattern.test(searchText);
+  if (patternMatch) {
+    errorTextEl.textContent = "Your search may not contain numbers";
+    errorEl.classList.add("error--visible");
+    setTimeout(() => {
+      errorEl.classList.remove("error--visible");
+    }, 3500);
+  }
+  //blur input
+  searchInputEl.blur();
+  jobListSearchEl.innerHTML = "";
+  spinnerSearchEl.classList.add("spinner--visible");
 
-    //fetch search results
-    fetch(`https://bytegrad.com/course-assets/js/2/api/jobs?search=${searchText}`)
-        .then(response => {
-            if (!response.ok) {
-                console.log('Something went wrong!');
-                return;
-            }
-            return response.json();
-        })
-        .then(data => {
-            //extract job items only
-            const { jobItems } = data;
+  //fetch search results
+  fetch(`https://bytegrad.com/course-assets/js/2/api/jobs?search=${searchText}`)
+    .then((response) => {
+      if (!response.ok) {
+        console.log("Something went wrong!");
+        return;
+      }
+      return response.json();
+    })
+    .then((data) => {
+      //extract job items only
+      const { jobItems } = data;
 
-            spinnerSearchEl.classList.remove('spinner--visible');
-            numberEl.textContent = jobItems.length;
-            
-            jobItems.slice(0, 7).forEach(jobItem => {
-                const newJobItemHTML = `
+      spinnerSearchEl.classList.remove("spinner--visible");
+      numberEl.textContent = jobItems.length;
+
+      jobItems.slice(0, 7).forEach((jobItem) => {
+        const newJobItemHTML = `
                     <li class="job-item">
                         <a class="job-item__link" href="${jobItem.id}">
                             <div class="job-item__badge">${jobItem.badgeLetters}</div>
@@ -80,25 +86,131 @@ const submitHandler = event => {
                         </a>
                     </li>
                 `;
-                jobListSearchEl.insertAdjacentHTML('beforeend', newJobItemHTML);
-            });
-        })
-        .catch(error => console.log(error));
+        jobListSearchEl.insertAdjacentHTML("beforeend", newJobItemHTML);
+      });
+    })
+    .catch((error) => console.log(error));
 };
-searchFormEl.addEventListener('submit', submitHandler);
+searchFormEl.addEventListener("submit", submitHandler);
 
 //Job list component
-const clickHandler = event => {
-    event.preventDefault();
+const clickHandler = (event) => {
+  event.preventDefault();
 
-    //get clicked job element
-    const jobItemEl = event.target.closest('.job-Item');
+  //get clicked job element
+  const jobItemEl = event.target.closest(".job-item");
 
-    // remove  the active class from previously active job item
-    document.querySelector('.job-item--active').classList.remove('.job-item--active');
+  // remove  the active class from previously active job item
 
-    // add active class
-    jobItemEl.classList.add('job-item--active');
+  document
+    .querySelector(".job-item--active")
+    ?.classList.remove("job-item--active");
+
+  // add active class
+  jobItemEl.classList.add("job-item--active");
+
+  // empty the job details section
+  jobDetailsContentEl.innerHTML = "";
+  spinnerJobDetailsEl.classList.add("spinner--visible");
+
+  // get the ID
+  const id = jobItemEl.children[0].getAttribute("href");
+
+  // fetch job item data
+  fetch(`https://bytegrad.com/course-assets/js/2/api/jobs/${id}`)
+    .then((response) => {
+      if (!response.ok) {
+        console.log("Something went wrong!");
+        return;
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const { jobItem } = data;
+      spinnerJobDetailsEl.classList.remove("spinner--visible");
+
+      const jobDetailsHTML = `
+                <img src="${
+                  jobItem.coverImgURL
+                }" alt="#" class="job-details__cover-img">
+
+                <a class="apply-btn" href="${
+                  jobItem.coverImgURL
+                }" target="_blank">Apply <i class="fa-solid fa-square-arrow-up-right apply-btn__icon"></i></a>
+
+                <section class="job-info">
+                    <div class="job-info__left">
+                        <div class="job-info__badge">${
+                          jobItem.badgeLetters
+                        }</div>
+                        <div class="job-info__below-badge">
+                            <time class="job-info__time">${
+                              jobItem.daysAgo
+                            }</time>
+                            <button class="job-info__bookmark-btn">
+                                <i class="fa-solid fa-bookmark job-info__bookmark-icon"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="job-info__right">
+                        <h2 class="second-heading">${jobItem.title}</h2>
+                        <p class="job-info__company">${jobItem.company}</p>
+                        <p class="job-info__description">${
+                          jobItem.description
+                        }</p>
+                        <div class="job-info__extras">
+                            <p class="job-info__extra"><i class="fa-solid fa-clock job-info__extra-icon"></i> ${
+                              jobItem.duration
+                            }</p>
+                            <p class="job-info__extra"><i class="fa-solid fa-money-bill job-info__extra-icon"></i> ${
+                              jobItem.salary
+                            }</p>
+                            <p class="job-info__extra"><i class="fa-solid fa-location-dot job-info__extra-icon"></i> ${
+                              jobItem.location
+                            }</p>
+                        </div>
+                    </div>
+                </section>
+
+                <div class="job-details__other">
+                    <section class="qualifications">
+                        <div class="qualifications__left">
+                            <h4 class="fourth-heading">Qualifications</h4>
+                            <p class="qualifications__sub-text">Other qualifications may apply</p>
+                        </div>
+                        <ul class="qualifications__list">
+                            ${jobItem.qualifications
+                              .map(
+                                (qualificationText) =>
+                                  `<li class="qualifications__item">${qualificationText}</li>`
+                              )
+                              .join("")}
+                        </ul>
+                    </section>
+
+                    <section class="reviews">
+                        <div class="reviews__left">
+                            <h4 class="fourth-heading">Company reviews</h4>
+                            <p class="reviews__sub-text">Recent things people are saying</p>
+                        </div>
+                        <ul class="reviews__list">
+                            ${jobItem.reviews
+                              .map(
+                                (reviewText) =>
+                                  `<li class="reviews__item">${reviewText}</li>`
+                              )
+                              .join("")}
+                        </ul>
+                    </section>
+                </div>
+
+                <footer class="job-details__footer">
+                  <p class="job-details__footer-text">If possible, please reference that you found the job on <span class="u-bold">rmtDev</span>, we would really appreciate it!</p>
+                </footer>
+            `;
+      jobDetailsContentEl.innerHTML = jobDetailsHTML;
+    })
+    .catch((error) => console.log(error));
 };
 
-jobListSearchEl.addEventListener('click', clickHandler);
+jobListSearchEl.addEventListener("click", clickHandler);
